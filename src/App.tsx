419c051f81fee9dd3d523227bf8055ad9a0da89f
sksachin7z2 <sksachin7z2@gmail.com>
2022-09-05 18:07:37 +0530

    sachin:login implementation done

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import { HomePage, LoginPage, ReviewThemes, SingleThemeReview } from "./pages";
 
 function App() {
   // TODO : Remove the sketchy login mechanism
-  !Cookies.get("auth_token") &&
-    Cookies.set(
-      "auth_token",
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMTc3ZDgzOTU3ODYyN2FhZDNlOWM5YyIsImlhdCI6MTY0NTcwNzE4MX0.wBhiGfT1-A3tsOGyQLCZSAHqCCUHbMNE1VtyClHxH7U"
-    );
+  // !Cookies.get("auth_token") &&
+  //   Cookies.set(
+  //     "auth_token",
+  //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMTc3ZDgzOTU3ODYyN2FhZDNlOWM5YyIsImlhdCI6MTY0NTcwNzE4MX0.wBhiGfT1-A3tsOGyQLCZSAHqCCUHbMNE1VtyClHxH7U"
+  //   );
   return (
     <>
       <Router>
